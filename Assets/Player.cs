93cc1639e3ddd68710efa2ad0d91326b2ec5f527
Octavio <octi124@hotmail.com>
2022-10-11 21:42:49 -0300

    Agregados atributos _board y _piecesManager a la clase Player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Board))]
+[RequireComponent(typeof(PiecesManager))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private string _name;
+
+    public Board _board;
+    public PiecesManager _piecesManager;
+
+    private void Awake()
     {
-        
+        _board = GetComponent<Board>();
+        _piecesManager = GetComponent<PiecesManager>();
     }
 
     // Update is called once per frame
