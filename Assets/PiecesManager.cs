a509b59952e6e46fea99bfc72b678df76c575b3c
Octavio <octi124@hotmail.com>
2022-10-11 22:18:00 -0300

    Agregado atributo a PieceManager

--- Assets/PiecesManager.cs
+++ Assets/PiecesManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PiecesManager : MonoBehaviour
 {
+    public PieceTile currentPiece;
+
     // Start is called before the first frame update
     void Start()
     {
