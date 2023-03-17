267977db331f29808df9f72fc78c1c42203382b8
Octavio <octavio.ahumada@hotmail.com>
2023-03-17 00:43:17 -0300

    Preparado un struct para poder poner espacios suficientes para todos los offsets de las piezas al girarlas

--- Assets/TetraPiece.cs
+++ Assets/TetraPiece.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class TetraPiece : Piece
 {
+    private int _currentPosition = 0;
 
+    [System.Serializable]
+    public struct Offsets
+    {
+        public Vector3Int[] _theOffsetClock;
+        public Vector3Int[] _theOffsetCounterClock;
+    }
+
+    [SerializeField]
+    private Offsets[] _positions;
 }
